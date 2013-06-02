ed18f8bab54753abdc7fe7ae4fc64bba688f2dca
Andy Park <sohocoke@gmail.com>
2013-06-02 15:15:52 +0200

    add evernote sandbox credentials.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
 		
-	 evernoteConsumerKey		: 'your consumer key'
-	,evernoteConsumerSecret	: 'your consumer secret'
-	,evernoteUsedSandbox		: false
+	 evernoteConsumerKey		: 'sohocoke'
+	,evernoteConsumerSecret	: '80af1fd7b40f65d0'
+	,evernoteUsedSandbox		: true
 	
 	,serverUrl:'http://localhost:8081'
 	,serverPort:8081,
