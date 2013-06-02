e6a16204f2b7c7a58178db3e953915943c5dd108
Andy Park <sohocoke@gmail.com>
2013-06-02 15:33:19 +0200

    set server settings for heroku.

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ module.exports = {
 	,evernoteConsumerSecret	: '80af1fd7b40f65d0'
 	,evernoteUsedSandbox		: true
 	
-	,serverUrl:'http://localhost:8081'
-	,serverPort:8081,
+	// ,serverUrl:'http://localhost:8081'
+	// ,serverPort:8081,
+	,serverUrl:'http://mackerel-everest.herokuapp.com'
+	,serverPort:80,
 }
