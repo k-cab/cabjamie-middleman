4a90b35b8b5d6026700f8d2b99eea7305241c901
Andy Park <sohocoke@gmail.com>
2013-06-20 23:30:54 +0200

    add Procfile

--- config.js
+++ config.js
@@ -1,12 +1,16 @@
 
 module.exports = {
-		
-	 evernoteConsumerKey		: 'sohocoke'
-	,evernoteConsumerSecret	: '80af1fd7b40f65d0'
-	,evernoteUsedSandbox		: true
-	
-	// ,serverUrl:'http://localhost:8081'
-	// ,serverPort:8081,
-	,serverUrl:'http://mackerel-everest.herokuapp.com'
-	,serverPort:80,
+    
+   evernoteConsumerKey    : 'sohocoke'
+  ,evernoteConsumerSecret : '80af1fd7b40f65d0'
+  ,evernoteUsedSandbox    : true
+  
+/*  
+  ,serverUrl:'http://localhost:8081'
+  ,serverPort:8081,
+
+*/  
+
+  ,serverUrl:'http://mackerel-everest.herokuapp.com'
+  ,serverPort:80,
 }
