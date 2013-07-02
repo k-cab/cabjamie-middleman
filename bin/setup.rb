e39d821a830110fe131d6d0f9ba68d6507e04a12
Andy Park <sohocoke@gmail.com>
2013-07-02 23:17:04 +0200

    bin/setup.rb, not tested.

---
+++ bin/setup.rb
@@ -0,0 +1,17 @@
+# prerequisites:
+## install xcode gcc installer
+## exclude dirs .bundle, vendor from dropbox sync
+
+
+# install rvm
+curl -L https://get.rvm.io | bash -s stable
+
+# # install brew
+# bin/install_homebrew.rb
+
+# install a ruby - will probably install macports.
+rvm install 1.9.3
+
+# install the bundle
+DEBUG_RESOLVER=1 bundle install --no-deployment
+
