bb446ecdd98594bc5191ca2771c87dda1be03bcc
Andy Park <sohocoke@gmail.com>
2013-07-24 00:02:39 +0200

    set prod config to use prod evernote

--- mackerel-everest/config.production.js
+++ mackerel-everest/config.production.js
@@ -3,7 +3,7 @@ module.exports = {
     
    evernoteConsumerKey    : 'sohocoke'
   ,evernoteConsumerSecret : '80af1fd7b40f65d0'
-  ,evernoteUsedSandbox    : true
+  ,evernoteUsedSandbox    : false
   
   ,serverUrl:'http://mackerel-everest.herokuapp.com'
   ,serverPort:80,
